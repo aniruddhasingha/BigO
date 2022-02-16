5cdc8949ce96a25663ba8be465d06ade1452f55d
Aniruddha Singha <aniruddhasingha95@gmail.com>
2022-02-16 05:49:25 +0000

    findNemo1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello BigO")
+const nemo = ['nemo'];
+
+function findNemo1(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!');
+    }
+  }
+}
+
+findNemo1(nemo);
