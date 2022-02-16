3d752d577dc6d44378b03d4e36b9bc5f2a08df8f
Aniruddha Singha <aniruddhasingha95@gmail.com>
2022-02-16 07:45:48 +0000

    O(n) linear time

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const large = new Array(10000).fill('nemo');
 function findNemo2(fish) {
   for (let i = 0; i < fish.length; i++) {
     if (fish[i] === 'nemo') {
-      console.log('Found NEMO!');
+      console.log('Found NEMO!'); 
     }
   }
 }
-
-findNemo2(large)
+// O(n)
+findNemo2(large) 
