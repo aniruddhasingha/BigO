9f12f01591a61b3eec43079752bf3276b3c89b71
Aniruddha Singha <aniruddhasingha95@gmail.com>
2022-02-16 12:10:19 +0000

    Drop the non dominant terms

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-// O(n*n)
-function printAllPairsOfArray(array) {
-  for(let i=0; i<array.length;i++){
-      for(let j=0; j<array.length;j++){
-    console.log(array[i],array[j]);
-  }
-  }
-}
-let a = [1,2,3,4,5];
+function printAllNumbersThenAllPairSums(numbers) {
 
-printAllPairsOfArray(a);
+  console.log('these are the numbers:');
+  numbers.forEach(function(number) {
+    console.log(number); 
+  });
+
+  console.log('and these are their sums:');
+  numbers.forEach(function(firstNumber) { 
+    numbers.forEach(function(secondNumber) {
+      console.log(firstNumber + secondNumber);
+    });
+  });
+}
+
+printAllNumbersThenAllPairSums([1,2,3,4,5])
+// O(n+n^2) = O(n^2);
