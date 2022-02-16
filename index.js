c46f949183ef6add4044d977c4f63af81e6cbb24
Aniruddha Singha <aniruddhasingha95@gmail.com>
2022-02-16 07:37:26 +0000

    Mark Down Added

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', '
 const large = new Array(10000).fill('nemo');
 
 function findNemo2(fish) {
-  let t0 = performance.now();
   for (let i = 0; i < fish.length; i++) {
     if (fish[i] === 'nemo') {
       console.log('Found NEMO!');
     }
   }
-  let t1 = performance.now();
-  console.log("Call to find Nemo took " + (t1 - t0) + " milliseconds.");
 }
 
 findNemo2(large)
