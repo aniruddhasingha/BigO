b3d6e656571091ac9a9617946675f8525787c5f4
Aniruddha Singha <aniruddhasingha95@gmail.com>
2022-02-16 10:42:36 +0000

    Constant time O(1)

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 //#1 -- For loop in Javascript.
-const fish = ['dory', 'bruce', 'marlin', 'nemo'];
-const nemo = ['nemo'];
 const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
-const large = new Array(10000).fill('nemo');
 
-function findNemo2(fish) {
-  for (let i = 0; i < fish.length; i++) {
-    if (fish[i] === 'nemo') {
-      console.log('Found NEMO!'); 
-    }
+
+function findNemo2(array) {
+  console.log(array[1]);
+  console.log(array[2]);
   }
-}
-// O(n)
-findNemo2(large) 
+// O(2)
+findNemo2(everyone) 
